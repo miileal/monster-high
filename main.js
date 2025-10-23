6a524ce2347ef9e03a47b405a3f13b912bd176b9
miileal <yasmin.dallazuana.santos@escola.pr.gov.br>
2025-10-23 10:12:18 -0300

    miga deve ser dificil ser vc np espelho

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const avanca = document.querySelectorAll('.btn-proximo')
 console.log(avanca)
 
 avanca.forEach(button => {
-    button.addEventListener()
+    button.addEventListener('click', function())
+    
 })
